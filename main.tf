89f4502e4c63e377bcaea0859ca16f3be5f6a26b
Tom Yandell <tom@tomyandell.co>
2017-09-20 17:11:42 +0000

    Add optional name suffix
    This will allow services to provide multiple copies of their service,
    each with a different suffix.

--- main.tf
+++ main.tf
@@ -20,11 +20,11 @@ module "ecs_update_monitor" {
 module "service" {
   source = "github.com/mergermarket/tf_load_balanced_ecs_service"
   
-  name            = "${var.env}-${lookup(var.release, "component")}"
+  name            = "${var.env}-${lookup(var.release, "component")}${var.name_suffix}"
   cluster         = "${var.ecs_cluster}"
   task_definition = "${module.taskdef.arn}"
   vpc_id          = "${var.platform_config["vpc"]}"
-  container_name  = "${lookup(var.release, "component")}"
+  container_name  = "${lookup(var.release, "component")}${var.name_suffix}"
   container_port  = "${var.port}"
   desired_count   = "${var.desired_count}"
 }
@@ -32,14 +32,14 @@ module "service" {
 module "taskdef" {
   source = "github.com/mergermarket/tf_ecs_task_definition"
 
-  family                = "${var.env}-${lookup(var.release, "component")}"
+  family                = "${var.env}-${lookup(var.release, "component")}${var.name_suffix}"
   container_definitions = ["${module.service_container_definition.rendered}"]
 }
 
 module "service_container_definition" {
   source = "github.com/mergermarket/tf_ecs_container_definition.git"
 
-  name           = "${lookup(var.release, "component")}"
+  name           = "${lookup(var.release, "component")}${var.name_suffix}"
   image          = "${lookup(var.release, "image_id")}"
   cpu            = "${var.cpu}"
   memory         = "${var.memory}"
@@ -47,8 +47,8 @@ module "service_container_definition" {
 
   container_env  = "${merge(
     map(
-      "LOGSPOUT_CLOUDWATCHLOGS_LOG_GROUP_STDOUT", "${var.env}-${lookup(var.release, "component")}-stdout",
-      "LOGSPOUT_CLOUDWATCHLOGS_LOG_GROUP_STDERR", "${var.env}-${lookup(var.release, "component")}-stderr",
+      "LOGSPOUT_CLOUDWATCHLOGS_LOG_GROUP_STDOUT", "${var.env}-${lookup(var.release, "component")}${var.name_suffix}-stdout",
+      "LOGSPOUT_CLOUDWATCHLOGS_LOG_GROUP_STDERR", "${var.env}-${lookup(var.release, "component")}${var.name_suffix}-stderr",
       "STATSD_HOST", "172.17.42.1",
       "STATSD_PORT", "8125",
       "STATSD_ENABLED", "true",
@@ -63,11 +63,11 @@ module "service_container_definition" {
 }
 
 resource "aws_cloudwatch_log_group" "stdout" {
-  name              = "${var.env}-${lookup(var.release, "component")}-stdout"
+  name              = "${var.env}-${lookup(var.release, "component")}${var.name_suffix}-stdout"
   retention_in_days = "7"
 }
 
 resource "aws_cloudwatch_log_group" "stderr" {
-  name              = "${var.env}-${lookup(var.release, "component")}-stderr"
+  name              = "${var.env}-${lookup(var.release, "component")}${var.name_suffix}-stderr"
   retention_in_days = "7"
 }
